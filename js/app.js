8df4167c104a4b3c906a8b6567af1c17ab57027f
Shivraj S <shivrajs.bayar@gmail.com>
2019-02-23 14:41:45 +0530

    Game logic done, clean up pending, winning UI pending, roatation to be corrected

--- js/app.js
+++ js/app.js
@@ -36,3 +36,81 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+// construct the the deck dom
+function buildDeck(deckArray) {
+    console.log(deckArray);
+    const deckFragment = document.createDocumentFragment();
+    let deckUlElement = document.querySelector('.deck');
+    let i = 0;
+    for(const deckArr of deckArray) {
+        const newLiElement = document.createElement('li');
+        const newIconElement = document.createElement('i');
+        const newSpanElement = document.createElement('span');
+        newSpanElement.innerText = ++i;
+        newSpanElement.style.display = 'none';
+        newSpanElement.className = 'dummy';
+        newLiElement.className = 'card';
+        newIconElement.className = deckArr;
+        newLiElement.appendChild(newIconElement);
+        newLiElement.appendChild(newSpanElement);
+        deckFragment.appendChild(newLiElement);
+    }
+    deckUlElement.appendChild(deckFragment);
+}
+
+const deckArray = ['fab fa-github', 'fab fa-android', 'fab fa-amazon', 'fab fa-google',
+                    'fab fa-facebook-f', 'fab fa-twitter', 'fab fa-instagram', 'fab fa-apple',
+                    'fab fa-github', 'fab fa-android', 'fab fa-amazon', 'fab fa-google',
+                    'fab fa-facebook-f', 'fab fa-twitter', 'fab fa-instagram', 'fab fa-apple'];
+buildDeck(shuffle(deckArray));
+
+const deckSelector = document.querySelector('.deck');
+const counterElement = document.querySelector('.moves');
+let clickCounter = 0;
+let matchCounter = 0;
+let failCounter = 0;
+let deckQueue = [];
+
+deckSelector.addEventListener('click', function(events) {
+    if(events && events.target && events.target.tagName.toLowerCase() === 'li') {
+        console.log(events);
+        if(deckQueue.length == 2)
+            return;
+
+        deckQueue.push(events.target);
+        ++clickCounter;
+        counterElement.textContent = clickCounter;
+        events.target.classList.add('open');
+        events.target.classList.add('show');
+
+        //if 2 cards are clicked and both are same cards
+        if(clickCounter % 2 === 0 
+            && events.target.firstElementChild.className ===  deckQueue[0].firstElementChild.className) {
+            //avoiding self clicks
+            if(events.target.querySelector('.dummy').innerText === deckQueue[0].querySelector('.dummy').innerText) {
+                --clickCounter;
+                return;
+            }
+            ++matchCounter;
+            deckQueue[0].classList.add('match');
+            events.target.classList.add('match');
+            deckQueue = [];
+
+            //winning logic
+            if(matchCounter * 2 === deckArray.length) {
+                console.log('Hip hip hurray!!!');
+            }
+        } else if(clickCounter % 2 === 0
+            && events.target.firstElementChild.className !==  deckQueue[0].firstElementChild.className) {
+            ++failCounter;
+            setTimeout(function(){ 
+                events.target.classList.remove('open');
+                events.target.classList.remove('show');
+                deckQueue[0].classList.remove('open');
+                deckQueue[0].classList.remove('show');
+                deckQueue = [];
+            }, 1000);
+        }
+    }
+});
